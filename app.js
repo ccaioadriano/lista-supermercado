093164b27b78730b0641ff2919298519dbb92eab
unknown <caiobhadriano@gmail.com>
2023-03-19 00:35:20 -0300

    Adicionando botao limpar carrinho

--- app.js
+++ app.js
@@ -2,14 +2,31 @@ var produtos = [];
 
 var button_submit = document.querySelector('input[type=submit]');
 
+var button_clear = document.querySelector('button[name=limpar]');
+
+
+
+
+
 button_submit.addEventListener('click', () => {
     let nomeProduto = document.querySelector('input[name=nome_produto]');
     let valorProduto = document.querySelector('input[name=valor_produto]');
 
-    produtos.push({
-        nome: nomeProduto.value,
-        preco: parseFloat(valorProduto.value),
-    })
+    while (isNaN(valorProduto.value)) {
+        alert('O valor precisa ser numérico');
+        nomeProduto.value = "";
+        valorProduto.value = "";
+        return
+    }
+
+    if ((nomeProduto.value && valorProduto.value)) {
+        produtos.push({
+            nome: nomeProduto.value,
+            preco: parseFloat(valorProduto.value),
+        })
+    } else {
+        alert('Favor inserir dados para cadastrar um produto');
+    }
 
 
     let listaProdutos = document.querySelector('.lista-produtos');
@@ -17,14 +34,15 @@ button_submit.addEventListener('click', () => {
     listaProdutos.innerHTML = "";
     let soma = 0;
 
-    
 
-    produtos.map((produto)=> {
-        listaProdutos.innerHTML+= `
+
+    produtos.map((produto) => {
+
+        listaProdutos.innerHTML += `
         
         <div class="lista-produtos-single">
-            <h3>`+produto.nome+`</h3>
-            <h3 class="price-produto"><span>R$ `+produto.preco.toFixed(2)+`</span></h3>
+            <h3>`+ produto.nome + `</h3>
+            <h3 class="price-produto"><span>R$ `+ produto.preco.toFixed(2) + `</span></h3>
         </div>
         
         `;
@@ -33,13 +51,23 @@ button_submit.addEventListener('click', () => {
 
         soma += produto.preco;
 
-        elementoTotal.innerHTML = 'Total: R$'+soma.toFixed(2);
+        elementoTotal.innerHTML = 'Total: R$' + soma.toFixed(2);
 
     });
 
     nomeProduto.value = "";
     valorProduto.value = "";
 
-
 });
+
+button_clear.addEventListener('click', ()=>{
+
+    produtos = [];
+
+    let listaProdutos = document.querySelector('.lista-produtos');
+    listaProdutos.innerHTML = "";
+
+    let elementoTotal = document.querySelector('.total h1');
+    elementoTotal.innerHTML = "";
+})
 
